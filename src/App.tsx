2d75d26e1955d6a46bf00d313afc49ce40747d42
Nir Geller <nir@aim.security>
2025-03-04 09:56:29 +0200

    Add aviv fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,31 @@ import PageLayout from './components/PageLayout'
 function App() {
   return (
     <ChakraProvider theme={customTheme}>
-      <PageLayout title="Create your own demo here! 📺">
+      <PageLayout title="Create Your Demo Here! 📺">
         <Flex height="100vh" width="100vw" justifyContent="center">
           <Flex direction="column" gap={2}>
-            <Text variant="SemiBold" size="TextLG">The following are instructions to bolt.new for how to build websites using this code base</Text>
-            <Text variant="SemiBold" size="TextMD">Ever page should be implemented with the PageLayout component as this page</Text>
-            <Flex gap={1} direction="column" justifyContent="center" width="100%">
-              <Text variant="SemiBold" size="TextMD">An example of the badge usage, use it for all the badges that you need!</Text>
+            <Text variant="SemiBold" size="TextMD">
+              The following are instructions to bolt.new for how to build
+              websites using this code base
+            </Text>
+            <Text variant="Regular" size="TextMD">
+              Ever page should be implemented with the PageLayout component as
+              this page
+            </Text>
+            <Flex
+              gap={1}
+              direction="column"
+              justifyContent="center"
+              width="100%"
+            >
+              <Text variant="Regular" size="TextSM">
+                An example of the badge usage, use it for all the badges that
+                you need!
+              </Text>
               <Flex gap={1}>
-                <AimBadge size="sm" text="success" color="green" />
-                <AimBadge size="sm" text="error" color="red" />
-                <AimBadge size="sm" text="warning" color="yellow" />
+                <AimBadge size="md" text="success" color="green" />
+                <AimBadge size="md" text="error" color="red" />
+                <AimBadge size="md" text="warning" color="yellow" />
               </Flex>
             </Flex>
           </Flex>
